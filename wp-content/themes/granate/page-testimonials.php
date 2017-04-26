8f6ef64906d14d56ed51abdc29f795db3b5c3e15
Ruslan Mayatskiy <the.ripper@icloud.com>
2017-04-26 12:29:37 +0300

    Установка disqus

--- wp-content/themes/granate/page-testimonials.php
+++ wp-content/themes/granate/page-testimonials.php
@@ -5,6 +5,13 @@
 		<div class="block-ttl left">
 			<?= get_the_title(); ?>
 		</div><!-- .block-ttl left -->
+		<div class="row">
+			<div class="col-lg-12">
+			<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+				<?php comments_template( ); ?>
+			<?php endwhile; endif; ?>
+			</div>
+		</div>
 	</div><!-- .container -->
 </div><!-- .contacts -->
 
