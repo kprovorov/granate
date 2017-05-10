76d80dc75d1d1d12a8232fc5c045c1ceab59f63e
Maksim <maksim.pavlenko19@gmail.com>
2017-05-10 16:57:08 +0300

    pop-up doctor sertificats

--- wp-content/themes/granate/page-doctors.php
+++ wp-content/themes/granate/page-doctors.php
@@ -42,7 +42,9 @@
 										<div class="row">
 											<?php foreach ($gallery as $image): ?>
 												<div class="col-lg-4">
-													<img src="<?= get_image_field( $image, "300-square" ) ?>" alt="doctor1" />
+													<a target="_blank" class="image-popup-no-margins" href="<?= get_image_field( $image, "300-square" ) ?>">
+														<img src="<?= get_image_field( $image, "300-square" ) ?>" alt="doctor1">
+													</a>
 												</div>
 											<?php endforeach; ?>
 
